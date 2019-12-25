17af0e15512d51ca1254d213427b3d2eaa4e25a2
Pavan Kumar <telanakulavss.pavan@hcl.com>
2019-12-25 18:42:36 +0530

    add the crud operations

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -5,39 +5,61 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/details")
 public class Controller {
-	
+
 	List<SuperMarket> theSuper;
+
 	@PostConstruct
 	public void loadData() {
 		theSuper = new ArrayList<>();
-		theSuper.add(new SuperMarket("SOAP","Rexona"));
+		theSuper.add(new SuperMarket("SOAP", "Rexona"));
 		theSuper.add(new SuperMarket("Oil", "GroundNut"));
-		theSuper.add(new SuperMarket("Paste","Dabur red"));
+		theSuper.add(new SuperMarket("Paste", "Dabur red"));
 	}
 
-	@GetMapping("//super")
-	public List<SuperMarket> getSuperMarketList(){
-		
+	
+
+	@GetMapping("/super")
+	public List<SuperMarket> getSuperMarketList() {
+
 		return theSuper;
 	}
-	@GetMapping("//super/{superid}")
-	public SuperMarket getsuper(@PathVariable int superid ) {
-		
-		if(superid >theSuper.size()||superid<0) {
-			throw new SuperNotFound("Unable to get the product--"+superid);
+	@PostMapping("/super")
+	public SuperMarket addSuperMarket(@RequestBody SuperMarket supermarket) {
+		theSuper.add(supermarket);
+		return supermarket;
+	}
+
+	@GetMapping("/super/{superid}")
+	public SuperMarket getsuper(@PathVariable int superid) {
+
+		if (superid > theSuper.size() || superid < 0) {
+			throw new SuperNotFound("Unable to get the product--" + superid);
 		}
 		return theSuper.get(superid);
+	}
+	@DeleteMapping("/super/{superid}")
+	public String deleteSuperproduct(@PathVariable int superid) {
+		SuperMarket supermarket = theSuper.get(superid);
+		theSuper.remove(supermarket);
+		
+		return "Deleted successfully";
+	}
+	@PutMapping("/super")
+	public SuperMarket UpdateSuperMarket(@RequestBody SuperMarket supermarket) {
+		theSuper.add(supermarket);
+		return supermarket;
 	}
 	
 
